4d840140124cd5e0351f885463a634acda715111
gabi <gleiva@unca.edu.ar>
2024-11-27 18:59:51 -0300

    agregando superheroes

--- sprint-1/Trabajo-Practico-3/utils.mjs
+++ sprint-1/Trabajo-Practico-3/utils.mjs
@@ -19,28 +19,51 @@ class Superheroe {
 export function leerSuperheroes(ruta) {
     const datos = fs.readFileSync(ruta, 'utf8');
     const superheroesArray = JSON.parse(datos)
-    
+
     const superheroes = superheroesArray.map(hero =>
-        new Superheroe(hero.id, hero.nombreSuperheroe, hero.nombreReal, hero.nombreSociedad, hero.edad, hero.planetaOrigen, hero.debilidad, hero.poder, hero.habilidadEspecial, hero.aliado, hero.enemigo)
+        new Superheroe(
+            hero.id,
+            hero.nombreSuperheroe,
+            hero.nombreReal,
+            hero.nombreSociedad,
+            hero.edad,
+            hero.planetaOrigen,
+            hero.debilidad,
+            hero.poder,
+            hero.habilidadEspecial,
+            hero.aliado,
+            hero.enemigo)
     )
     console.log(superheroes);
-    superheroes.sort((a,b)=>a.nombreSuperheroe.localeCompare(b.nombreSuperheroe));
-    
+    superheroes.sort((a, b) => a.nombreSuperheroe.localeCompare(b.nombreSuperheroe));
+
     return superheroes
 }
 
-export function agregarSuperHeroes(rutaOriginal, rutaNuevos){
-    const datosOriginales =fs.readFileSync(rutaOriginal,'utf8')
-    const datosNuevos =fs.readFileSync(rutaNuevos,'utf8')
+export function agregarSuperHeroes(rutaOriginal, rutaNuevos) {
+    const datosOriginales = fs.readFileSync(rutaOriginal, 'utf8')
+    const datosNuevos = fs.readFileSync(rutaNuevos, 'utf8')
 
     const superheroesOriginales = JSON.parse(datosOriginales)
     const nuevosSuperheroes = JSON.parse(datosNuevos)
 
     const instanciasNuevos = nuevosSuperheroes.map(hero =>
-        new Superheroe(hero.id, hero.nombreSuperheroe, hero.nombreReal, hero.nombreSociedad, hero.edad, hero.planetaOrigen, hero.debilidad, hero.poder, hero.habilidadEspecial, hero.aliado, hero.enemigo))
+        new Superheroe(
+            hero.id,
+            hero.nombreSuperheroe,
+            hero.nombreReal,
+            hero.nombreSociedad,
+            hero.edad,
+            hero.planetaOrigen,
+            hero.debilidad,
+            hero.poder,
+            hero.habilidadEspecial,
+            hero.aliado,
+            hero.enemigo)
+        )
 
-    const listaActulizada = [...superheroesOriginales,...instanciasNuevos]
+    const listaActulizada = [...superheroesOriginales, ...instanciasNuevos]
 
-    fs.writeFileSync(rutaOriginal,JSON.stringify(listaActulizada,null,2),'utf8');
+    fs.writeFileSync(rutaOriginal, JSON.stringify(listaActulizada, null, 2), 'utf8');
     console.log('Lista de superheroes actualizada con exito');
 }
